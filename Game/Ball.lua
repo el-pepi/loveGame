1ce00a58bf54eecff262a49990f369bfad368a08
28 <28>
2017-06-19 21:34:12 -0300

    ball phys + score

--- Game/Ball.lua
+++ Game/Ball.lua
@@ -1,3 +1,4 @@
+ballSize = 1
 newball = function()
 	ball = {}
 	ball.x = 500
@@ -12,22 +13,44 @@ newball = function()
 	ball.width = ball.image:getWidth()
 	ball.height = ball.image:getHeight()
 	
+	ball.rot = 0
+	
+	ballSize = ballStartSize
+	
 	ball.update = function(dt)
 		ball.vel.y = ball.vel.y + dt * gravity
 		
 		ball.y = ball.y + ball.vel.y
+		if ball.y > ground then
+			ball.y = ground
+			ball.vel.y = ball.vel.y * -0.5
+			score = 0
+			ballSize = ballStartSize
+		end
+		if ball.x > rightWall then
+			ball.x = rightWall
+			ball.vel.x = ball.vel.x * -0.5
+		end
+		if ball.x < leftWall then
+			ball.x = leftWall
+			ball.vel.x = ball.vel.x * -0.5
+		end
 		
 		ball.x = ball.x + ball.vel.x * dt;
+		
+		ball.rot = ball.rot + ball.vel.x * dt;
 	end
 	
 	ball.draw = function()
-		love.graphics.draw(ball.image, ball.x, ball.y, 0, 1, 1, ball.width/2, ball.height/2)
+		love.graphics.draw(ball.image, ball.x, ball.y, ball.rot * 0.01, ballSize, ballSize, ball.width/2, ball.height/2)
 	end
 	
 	ball.onTap = function(x,y)
-		if math.dist(x, y, ball.x, ball.y) < 65 then
+		if math.dist(x, y, ball.x, ball.y) < (ball.width * ballSize)/2 then
 			ball.vel.y = -ballHitForce
 			ball.vel.x = (ball.x - x) * 20
+			score = score + 1
+			ballSize = ballSize - 0.05
 		end
 	end
 	
--- Game/Main.lua
+++ Game/Main.lua
@@ -4,8 +4,19 @@ gravity = 20
 
 ballHitForce = 15
 
+ground = 500
+leftWall = 20
+rightWall = 700
+
+score = 0
+
+font = love.graphics.newFont(30)
+
+ballStartSize = 1.3
+
 function love.load()
 ball = newball()
+love.graphics.setFont(font)
 end
 
 function love.update(dt)
@@ -14,6 +25,7 @@ end
 
 function love.draw()
 ball.draw()
+love.graphics.print('score: ' .. score, 0, 0)
 end
 
 function love.mousepressed(x,y)
